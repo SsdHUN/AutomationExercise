e711d803b0a77a4c06eb2233c638e9849282d885
SsdHUN <santhasoma1999@gmail.com>
2023-06-06 12:38:41 +0200

    implemet: Find used element and create there basic methods.

--- src/java/PageFactory/LoginPage.java
+++ src/java/PageFactory/LoginPage.java
@@ -1,7 +1,55 @@
 package PageFactory;
 
 
-public class LoginPage {
+import Util.Util;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import static Util.Util.*;
+
+public class LoginPage extends BasePage {
+    public LoginPage(){
+        super();
+    }
+
+    @FindBy(xpath = "//a[contains(text(), 'Login')]")
+    WebElement loginRegisterButten;
+    @FindBy(xpath = "//h2[contains(text(), 'New User Signup!')]")
+    WebElement singupText;
+    @FindBy(xpath = "//input[@data-qa='signup-name']")
+    WebElement singUoName;
+    @FindBy(xpath = "//input[@data-qa='signup-email']")
+    WebElement singUpEmail;
+    @FindBy(xpath = "//button[@data-qa='signup-button']")
+    WebElement singUpBtn;
 
 
+    public void clickLoginRegisterBtn(){
+        wait.until(ExpectedConditions.visibilityOf(loginRegisterButten));
+        loginRegisterButten.click();
+    }
+    public void waitToPresentSingupText(){
+        wait.until(ExpectedConditions.visibilityOf(singupText));
+    }
+    public void navigateToHomePage(){
+
+        driver.get(BASE_URL);
+    }
+    public void fillSingUpName(String name){
+        singUoName.sendKeys(name);
+    }
+    public void fillSingUpEmail(String email){
+        singUpEmail.sendKeys(email);
+    }
+    public void clickSingUpBtn(){
+        singUpBtn.click();
+    }
+
+    public void navigateToLoginPage(){
+        driver.get(BASE_URL+"/login");
+    }
+    public void urlprint(){
+        System.out.println(Util.BASE_URL);
+    }
 }
